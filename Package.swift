2f8d5c197b3555955342ae70ed14edc35f590f3d
caleb <caleb@benefic.com>
2023-12-26 22:11:07 -0700

    Moving things around

--- Package.swift
+++ Package.swift
@@ -7,13 +7,11 @@ let package = Package(
     name: "word2vec",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(name: "NLPPOS", targets: ["NLPPOS"]),
         .executable(name: "NLPTrain", targets: ["NLPTrain"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "NLPPOS"),
-        .executableTarget(name: "NLPTrain", dependencies: ["NLPPOS"])
+        .executableTarget(name: "NLPTrain", dependencies: [])
     ]
 )
